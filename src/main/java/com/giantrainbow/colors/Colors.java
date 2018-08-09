bdd4768969c775beea7ac0a3393ecf4ff51b0b8a
Shawn Silverman <shawn@pobox.com>
2018-08-08 23:18:35 -0700

    Reformatting Colors. :P

--- src/main/java/com/giantrainbow/colors/Colors.java
+++ src/main/java/com/giantrainbow/colors/Colors.java
@@ -30,30 +30,36 @@ public class Colors {
 
   // https://simple.wikipedia.org/wiki/Rainbow
   public static final int[] RAINBOW_PALETTE = {
-    RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET,
+      RED,
+      ORANGE,
+      YELLOW,
+      GREEN,
+      BLUE,
+      VIOLET,
   };
 
   // https://www.webnots.com/vibgyor-rainbow-color-codes/
   public static final int[] RAINBOW2_PALETTE = {
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    BLUE,
-    0xff4b0082, // Indigo
-    0xff9400d3, // Violet
+      RED,
+      ORANGE,
+      YELLOW,
+      GREEN,
+      BLUE,
+      0xff4b0082,  // Indigo
+      0xff9400d3,  // Violet
   };
 
   // http://www.color-hex.com/color-palette/7094
   public static final int[] RAINBOW3_PALETTE = {
-    0xffff1a00, // Red
-    0xffff8d00, // Orange
-    0xffe3ff00, // Yellow
-    0xff00ff04, // Green
-    0xff0051ff, // Blue
+      0xffff1a00,  // Red
+      0xffff8d00,  // Orange
+      0xffe3ff00,  // Yellow
+      0xff00ff04,  // Green
+      0xff0051ff,  // Blue
   };
 
-  private Colors() {}
+  private Colors() {
+  }
 
   /**
    * Chooses a random n-bit color. If {@code bits} is not in the range 1-8 then 8 will be assumed.
@@ -73,32 +79,44 @@ public class Colors {
     return c;
   }
 
-  /** Returns the red part of a 32-bit RGBA color. */
+  /**
+   * Returns the red part of a 32-bit RGBA color.
+   */
   public static int red(int color) {
     return (color >> 16) & 0xff;
   }
 
-  /** Returns the green part of a 32-bit RGBA color. */
+  /**
+   * Returns the green part of a 32-bit RGBA color.
+   */
   public static int green(int color) {
     return (color >> 8) & 0xff;
   }
 
-  /** Returns the blue part of a 32-bit RGBA color. */
+  /**
+   * Returns the blue part of a 32-bit RGBA color.
+   */
   public static int blue(int color) {
     return color & 0xff;
   }
 
-  /** Returns the alpha part of a 32-bit RGBA color. */
+  /**
+   * Returns the alpha part of a 32-bit RGBA color.
+   */
   public static int alpha(int color) {
     return (color >> 24) & 0xff;
   }
 
-  /** Returns a color constructed from the three components. The alpha component is set to 255. */
+  /**
+   * Returns a color constructed from the three components. The alpha component is set to 255.
+   */
   public static int rgb(int r, int g, int b) {
     return 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
   }
 
-  /** Returns a color constructed from the three components. The alpha component is set to 255. */
+  /**
+   * Returns a color constructed from the three components. The alpha component is set to 255.
+   */
   public static int hsb(float h, float s, float b) {
     return Color.HSBtoRGB(h, s, b);
   }
