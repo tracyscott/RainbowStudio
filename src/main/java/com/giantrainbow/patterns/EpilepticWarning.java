6c27524bb89805c70e427f0632a5c127d3702596
Joshua MacDonald <jmacd@lightstep.com>
2018-08-22 11:49:36 -0700

    New Epilepsy pattern

---
+++ src/main/java/com/giantrainbow/patterns/EpilepticWarning.java
@@ -0,0 +1,23 @@
+package com.giantrainbow.patterns;
+
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+
+@LXCategory(LXCategory.FORM)
+public class EpilepticWarning extends AbstractEpileptic {
+
+  public EpilepticWarning(LX lx) {
+    super(lx);
+  }
+
+  int patterns[][] = {
+    {0, 1, 2, 3},
+    {4, 5, 6, 7},
+    {8, 1, 9, 6, 8, 6, 9, 1},
+  };
+
+  int[] getPositions(int period) {
+    int idx = period % patterns.length;
+    return patterns[idx];
+  }
+};
