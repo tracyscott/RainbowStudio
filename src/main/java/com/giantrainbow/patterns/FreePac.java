b0f8f99ccf907f9a049174aebbfb32fc792f81ef
jmacd <jmacd@lightstep.com>
2019-10-07 00:07:45 -0700

    Free Pac

--- src/main/java/com/giantrainbow/patterns/FreePac.java
+++ src/main/java/com/giantrainbow/patterns/FreePac.java
@@ -4,7 +4,7 @@ import static processing.core.PConstants.PI;
 import static processing.core.PConstants.BOTTOM;
 import static processing.core.PConstants.CENTER;
 
-// TODO: Chasing Trucks
+import com.giantrainbow.model.space.Lissajous;
 
 import com.giantrainbow.colors.Colors;
 import com.giantrainbow.model.space.Space3D;
@@ -25,6 +25,15 @@ import processing.core.PFont;
 
 @LXCategory(LXCategory.FORM)
 public class FreePac extends CanvasPattern2D {
+  public final static String[] topSeven = {
+      "Free Iraq",
+      "Free Kashmir",
+      "Free Pacman",
+      "Free Palestine",
+      "Free People",
+      "Free Tibet",
+      "Free Ukraine",
+  };
 
   public static final int MAX_FONT_SIZE = 120;
 
@@ -43,20 +52,40 @@ public class FreePac extends CanvasPattern2D {
 
   PFont font;
 
-  public static final float lowRadius = RainbowBaseModel.innerRadius - 0 * RainbowBaseModel.radiusInc;
-  public static final float highRadius = RainbowBaseModel.innerRadius + 30 * RainbowBaseModel.radiusInc;
+  public static final float lowRadius =
+      RainbowBaseModel.innerRadius - 0 * RainbowBaseModel.radiusInc;
+  public static final float highRadius =
+      RainbowBaseModel.innerRadius + 30 * RainbowBaseModel.radiusInc;
 
   // Note these are in degrees.
   //   RainbowBaseModel.rainbowThetaStart;
   //   RainbowBaseModel.rainbowThetaFinish;
-  static final float rainbowAngleStart = (float) Math.toRadians(RainbowBaseModel.rainbowThetaStart);
-  static final float rainbowAngleFinish = (float) Math.toRadians(RainbowBaseModel.rainbowThetaFinish);
+  static final float rainbowAngleStart =
+      (float) Math.toRadians(RainbowBaseModel.rainbowThetaStart);
+  static final float rainbowAngleFinish =
+      (float) Math.toRadians(RainbowBaseModel.rainbowThetaFinish);
 
   int minRows = 1;
     
   public void onActive() {
     super.onActive();
   }
+
+  class Letter {
+    char c;
+
+      // TODO Random theta, random height as initial position
+      // TODO it will move w/ various speed around random Lissajous curve.
+      // TODO it will have brownian motion
+      // TODO except that it will experience a pull toward its home position
+      // TODO one that strengthens as a function of time according to schedule
+      // TODO consider compound Lissajous yesss
+      // TODO need to think about color
+      
+      Letter(char c) {
+	  this.c = c;
+      }
+  };
 
   public void draw(double deltaMs) {
       pg.background(0);
@@ -67,22 +96,26 @@ public class FreePac extends CanvasPattern2D {
       pg.textSize((float)sizeKnob.getValue());
       pg.textAlign(CENTER, BOTTOM);
 
-      for (int i = 0;
-	   i < STEPS;
-	   i++) {
-	  float theta = rainbowAngleStart + (i / (float)(STEPS-1)) * (rainbowAngleFinish - rainbowAngleStart);
 
-	  pg.pushMatrix();
-	  pg.translate(canvas.map.subXi((float)Math.cos(theta) * lowRadius),
-		       canvas.map.subYi((float)Math.sin(theta) * lowRadius));
-	  pg.fill(255,255,255);
-	  pg.rotate(theta+PI/2);
+
+
+
+      // for (int i = 0;
+      // 	   i < STEPS;
+      // 	   i++) {
+      // 	  float theta = rainbowAngleStart + (i / (float)(STEPS-1)) * (rainbowAngleFinish - rainbowAngleStart);
+
+      // 	  pg.pushMatrix();
+      // 	  pg.translate(canvas.map.subXi((float)Math.cos(theta) * lowRadius),
+      // 		       canvas.map.subYi((float)Math.sin(theta) * lowRadius));
+      // 	  pg.fill(255,255,255);
+      // 	  pg.rotate(theta+PI/2);
 
 	  
 
-	  pg.text(String.format("%s", (i < 26) ? (char)('a'+i) : (char)('A'+(i-26))), 0, 0);
+      // 	  pg.text(String.format("%s", (i < 26) ? (char)('a'+i) : (char)('A'+(i-26))), 0, 0);
 
-	  pg.popMatrix();
-      }
+      // 	  pg.popMatrix();
+      // }
   }
 }
