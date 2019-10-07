6ab73b2fc845bd93ba7210cca0700846187596e1
jmacd <jmacd@lightstep.com>
2019-10-06 23:08:44 -0700

    N=52

--- src/main/java/com/giantrainbow/patterns/FreePac.java
+++ src/main/java/com/giantrainbow/patterns/FreePac.java
@@ -4,8 +4,7 @@ import static processing.core.PConstants.PI;
 import static processing.core.PConstants.BOTTOM;
 import static processing.core.PConstants.CENTER;
 
-import static com.giantrainbow.model.RainbowBaseModel.rainbowThetaStart;
-import static com.giantrainbow.model.RainbowBaseModel.rainbowThetaFinish;
+// TODO: Chasing Trucks
 
 import com.giantrainbow.colors.Colors;
 import com.giantrainbow.model.space.Space3D;
@@ -29,6 +28,8 @@ public class FreePac extends CanvasPattern2D {
 
   public static final int MAX_FONT_SIZE = 120;
 
+  public static final int STEPS = 52;
+
   public final CompoundParameter sizeKnob =
       new CompoundParameter("FontSize", 68.20, 10, MAX_FONT_SIZE).setDescription("FontSize");
 
@@ -45,6 +46,14 @@ public class FreePac extends CanvasPattern2D {
   public static final float lowRadius = RainbowBaseModel.innerRadius - 0 * RainbowBaseModel.radiusInc;
   public static final float highRadius = RainbowBaseModel.innerRadius + 30 * RainbowBaseModel.radiusInc;
 
+  // Note these are in degrees.
+  //   RainbowBaseModel.rainbowThetaStart;
+  //   RainbowBaseModel.rainbowThetaFinish;
+  static final float rainbowAngleStart = (float) Math.toRadians(RainbowBaseModel.rainbowThetaStart);
+  static final float rainbowAngleFinish = (float) Math.toRadians(RainbowBaseModel.rainbowThetaFinish);
+
+  int minRows = 1;
+    
   public void onActive() {
     super.onActive();
   }
@@ -58,26 +67,21 @@ public class FreePac extends CanvasPattern2D {
       pg.textSize((float)sizeKnob.getValue());
       pg.textAlign(CENTER, BOTTOM);
 
-      for (int i = 0; i < 26; i++) {
-	  float theta = PI * 2 * i / 26;
+      for (int i = 0;
+	   i < STEPS;
+	   i++) {
+	  float theta = rainbowAngleStart + (i / (float)(STEPS-1)) * (rainbowAngleFinish - rainbowAngleStart);
+
 	  pg.pushMatrix();
 	  pg.translate(canvas.map.subXi((float)Math.cos(theta) * lowRadius),
 		       canvas.map.subYi((float)Math.sin(theta) * lowRadius));
 	  pg.fill(255,255,255);
 	  pg.rotate(theta+PI/2);
-	  pg.text(String.format("%s", (char)('a' + i)), 0, 0);
-	  pg.fill(255,0,0);
-	  pg.ellipse(0, 0, 10, 10);
-	  pg.popMatrix();
-      }
 
-      for (int i = 0; i < 26; i++) {
-	  float theta = PI * 2 * i / 26;
-	  pg.pushMatrix();
-	  pg.translate(canvas.map.subXi((float)Math.cos(theta) * highRadius),
-		       canvas.map.subYi((float)Math.sin(theta) * highRadius));
-	  pg.fill(255,0,0);
-	  pg.ellipse(0, 0, 10, 10);
+	  
+
+	  pg.text(String.format("%s", (i < 26) ? (char)('a'+i) : (char)('A'+(i-26))), 0, 0);
+
 	  pg.popMatrix();
       }
   }
