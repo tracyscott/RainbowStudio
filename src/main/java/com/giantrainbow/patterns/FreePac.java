8a772294ce2b10740811a4e3086fca38d79c2253
jmacd <jmacd@lightstep.com>
2019-10-09 00:10:16 -0700

    Bezier motion

--- src/main/java/com/giantrainbow/patterns/FreePac.java
+++ src/main/java/com/giantrainbow/patterns/FreePac.java
@@ -2,6 +2,7 @@ package com.giantrainbow.patterns;
 
 import static processing.core.PConstants.PI;
 import static processing.core.PConstants.BOTTOM;
+import static processing.core.PConstants.TOP;
 import static processing.core.PConstants.CENTER;
 
 import com.giantrainbow.model.space.Lissajous;
@@ -65,16 +66,21 @@ public class FreePac extends CanvasPattern2D {
 	0.001031250171,  // J
 	0.0007021277763, // Z
     };
+
+    public static final int HZ = 1000;
     
     public static final int MAX_FONT_SIZE = 120;
 
     public static final int NUM_LETTERS = 120;
     
-    public static final float THETA_RANGE = PI / 16f;  // in radians
+    public static final float THETA_RANGE = PI / 10f;  // in radians
 
     public final CompoundParameter sizeKnob =
 	new CompoundParameter("FontSize", 68.20, 10, MAX_FONT_SIZE).setDescription("FontSize");
 
+    public final CompoundParameter rotateKnob =
+	new CompoundParameter("Rotate", 0, 0, 2*PI).setDescription("Rotate");
+    
     // Weird. Without this the image is not cenetered on the visible
     // region?  Fix it here.
     public final CompoundParameter xshiftKnob =
@@ -101,11 +107,13 @@ public class FreePac extends CanvasPattern2D {
     Letter [][]letters;
     ArrayList<Letter> depth;
     Random rnd = new Random();
+    double elapsed;
     
     public FreePac(LX lx) {
 	super(lx);
 	addParameter(sizeKnob);
 	addParameter(xshiftKnob);
+	addParameter(rotateKnob);
 	removeParameter(fpsKnob);
 
 	this.font = RainbowStudio.pApplet.createFont("fonts/Roboto/Roboto-Regular.ttf",
@@ -114,6 +122,7 @@ public class FreePac extends CanvasPattern2D {
 	this.letters = new Letter[26][];
 	this.depth = new ArrayList<Letter>();
 
+	out:
 	for (int l = 0; l < 26; l++) {
 	    int num = Math.max(1, (int)(NUM_LETTERS * letterFreqs[l]));
 
@@ -122,6 +131,9 @@ public class FreePac extends CanvasPattern2D {
 	    for (int i = 0; i < num; i++) {
 		letters[l][i] = new Letter((char)('A' + l));
 		depth.add(letters[l][i]);
+		if (depth.size() >= NUM_LETTERS) {
+		    break out;
+		}
 	    }
 	}
 	Collections.shuffle(depth);
@@ -135,6 +147,8 @@ public class FreePac extends CanvasPattern2D {
 	double X;
 	double Y;
 
+	private Point() {}
+	
 	Point(double theta, double radius) {
 	    this.theta = theta;
 	    this.radius = radius;
@@ -142,48 +156,99 @@ public class FreePac extends CanvasPattern2D {
 	    this.Y = Math.sin(theta) * radius;
 	}
 
-	double heading(Point to) {
-	    double dot = to.X * X + to.Y * Y;
-	    double lenMe = Math.sqrt(X * X + Y * Y);
-	    double lenTo = Math.sqrt(to.X * to.X + to.Y * to.Y);
-	    return Math.acos(dot / (lenMe * lenTo));
+	double heading() {
+	    return Math.atan2(Y, X);
 	}
+
+	// double heading(Point to) {
+	//     double vx = to.X - X;
+	//     double vy = to.Y - Y;
+	//     // TODO
+	//     return Math.atan(vy / vx);
+	// }
+	
+	Point sub(Point a) {
+	    Point r = new Point();
+	    r.X = X - a.X;
+	    r.Y = Y - a.Y;
+	    // Note: theta and radius not computed
+	    return r;
+	}
+
+	Point add(Point a) {
+	    Point r = new Point();
+	    r.X = X + a.X;
+	    r.Y = Y + a.Y;
+	    // Note: theta and radius not computed
+	    return r;
+	}
+
+	Point scale(double s) {
+	    X *= s;
+	    Y *= s;
+	    // Note: modify in place
+	    return this;
+	}
+
+	// void setThetaRadius() {
+	//     theta = Math.atan(Y / X);
+	//     radius = Math.sqrt(X * X + Y * Y);
+	// }
     }
 
     class Letter {
 	String ch;
-	double t;
 	int color;
-	Point P0, P1, P2;
-      
-	// TODO Random theta, random height as initial position
-	// TODO it will move w/ various speed around random Lissajous curve.
-	// TODO it will have brownian motion
-	// TODO except that it will experience a pull toward its home position
-	// TODO one that strengthens as a function of time according to schedule
-	// TODO consider compound Lissajous yesss
-	// TODO need to think about color
-      
+	Point P, H, P0, P1, P2;
+
 	Letter(char ch) {
 	    this.ch = String.format("%s", ch);
-	    this.t = 0;
 	    this.P0 = randPos();
 	    this.P1 = randPosNear(this.P0);
-	    // this.P2 = randPos(this.P1, this.P0);
+	    this.P2 = randPosNear(this.P1);
 	    this.color = randColor();
+	}
+
+	void advance() {
+	    double t = elapsed % 1.;
+	    // System.err.println("ADVANCE " + String.format("%.10f", t));
+	    double tt1 = (1 - t)*(1 - t);
+	    double tt0 = t*t;
+
+	    this.P = P1.add(P0.sub(P1).scale(tt1)).add(P2.sub(P1).scale(tt0));
+	    this.H = P1.sub(P0).scale(2*(1-t)).add(P2.sub(P1).scale(2*t));
 	}
 
 	void draw() {
 	    pg.pushMatrix();
-	    pg.translate(canvas.map.subXi((float)(this.P0.X + xshiftKnob.getValue())),
-			 canvas.map.subYi((float)this.P0.Y));
+	    pg.translate(canvas.map.subXi((float)(this.P.X)),
+			 canvas.map.subYi((float)this.P.Y));
 	    pg.fill(this.color);
 
-	    pg.rotate((float)P0.heading(P1));
-	    
-	    pg.text(ch, 0, 0);
+	    pg.rotate((float)(this.H.heading() + PI/2));
 
-	    pg.popMatrix();	    
+	    pg.pushMatrix();
+	    pg.translate(0, (float)(sizeKnob.getValue()/2.));
+	    pg.text(ch, 0, 0);
+	    pg.popMatrix();
+
+	    pg.popMatrix();
+
+	    // pg.stroke(255);
+	    // pg.strokeWeight(2);
+
+	    // pg.fill(255, 0, 0);
+	    // pg.ellipse(canvas.map.subXi((float)P.X), canvas.map.subYi((float)P.Y), 5, 5);
+
+	    // pg.fill(255);
+	    // pg.ellipse(canvas.map.subXi((float)P0.X), canvas.map.subYi((float)P0.Y), 5, 5);
+	    // pg.ellipse(canvas.map.subXi((float)P1.X), canvas.map.subYi((float)P1.Y), 5, 5);
+	    // pg.ellipse(canvas.map.subXi((float)P2.X), canvas.map.subYi((float)P2.Y), 5, 5);
+
+	    // pg.line(canvas.map.subXi((float)P0.X), canvas.map.subYi((float)P0.Y),
+	    // 	    canvas.map.subXi((float)P1.X), canvas.map.subYi((float)P1.Y));
+	    // pg.line(canvas.map.subXi((float)P1.X), canvas.map.subYi((float)P1.Y),
+	    // 	    canvas.map.subXi((float)P2.X), canvas.map.subYi((float)P2.Y));
 	}
     };
 
@@ -200,14 +265,19 @@ public class FreePac extends CanvasPattern2D {
 
     public Point randPosNear(Point near) {
 	double lowT = Math.max(rainbowAngleStart, near.theta - THETA_RANGE/2);
-	double highT = Math.max(rainbowAngleFinish, near.theta + THETA_RANGE/2);
+	double highT = Math.min(rainbowAngleFinish, near.theta + THETA_RANGE/2);
 	double newTheta = lowT + (highT - lowT) * rnd.nextDouble();
+
+	// System.err.println("Near theta " + String.format("%.10f", near.theta) +
+	// 		   "New  theta " + String.format("%.10f", newTheta));
 
 	return new Point(newTheta, lowRadius + rnd.nextDouble() * rangeRadius);
     }
 
     public void draw(double deltaMs) {
+	elapsed += deltaMs / HZ;
 	pg.background(0);
+	pg.translate((float)xshiftKnob.getValue(), 0);
 
 	if (font != null) {
 	    pg.textFont(font);
@@ -216,6 +286,7 @@ public class FreePac extends CanvasPattern2D {
 	pg.textAlign(CENTER, BOTTOM);
 
 	for (Letter l : depth) {
+	    l.advance();
 	    l.draw();
 	}
     }
