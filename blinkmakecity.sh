9536c3e1cfdea18c45c2786574eb34cb3caff66c
Soma Holiday <soma@somaholiday.com>
2019-10-10 14:34:14 -0400

    Blink panel mapping : shell scripts

---
+++ blinkmakecity.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp blinkpanelconfig12city.json panelconfig12.json
+cp blinkpanelconfig16city.json panelconfig16.json
+cp blinkpixliteconfigcity.json pixliteconfig.json
---
+++ blinkmakeman.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp blinkpanelconfig12man.json panelconfig12.json
+cp blinkpanelconfig16man.json panelconfig16.json
+cp blinkpixliteconfigman.json pixliteconfig.json
