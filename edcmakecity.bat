e3abd3486637ca3db58824d806c11890ca66a1c6
Soma Holiday <soma@somaholiday.com>
2019-11-02 19:30:52 -0700

    Remove old config and show files

--- edcmakecity.bat
+++
@@ -1,3 +0,0 @@
-copy edcpanelconfig12city.json panelconfig12.json
-copy edcpanelconfig16city.json panelconfig16.json
-copy edcpixliteconfigcity.json pixliteconfig.json
--- edcmakeman.bat
+++
@@ -1,3 +0,0 @@
-copy edcpanelconfig12man.json panelconfig12.json
-copy edcpanelconfig16man.json panelconfig16.json
-copy edcpixliteconfigman.json pixliteconfig.json
