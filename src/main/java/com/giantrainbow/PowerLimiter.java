4c3c3996739cb70730087647de6fbf36636a0e26
Tracy Scott <tracy.scott@gmail.com>
2021-03-21 19:43:09 -0700

    Log the current active channels and active patterns when a clip  is detected.

--- src/main/java/com/giantrainbow/PowerLimiter.java
+++ src/main/java/com/giantrainbow/PowerLimiter.java
@@ -2,8 +2,7 @@ package com.giantrainbow;
 
 import com.giantrainbow.colors.Colors;
 import com.giantrainbow.patterns.RenderImageUtil;
-import heronarts.lx.LX;
-import heronarts.lx.LXEffect;
+import heronarts.lx.*;
 import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
 
@@ -27,6 +26,36 @@ public class PowerLimiter extends LXEffect {
     addParameter(clipDetect);
   }
 
+  /**
+   * We need to track down which patterns are playing
+   * @return
+   */
+  public void logActiveChannelNames() {
+    for (LXChannelBus channelBus : lx.engine.channels) {
+
+      if (channelBus instanceof LXGroup && channelBus.enabled.getValueb() && channelBus.fader.getValuef() > 0.1) {
+        LXGroup g = (LXGroup) channelBus;
+        if (g.channels.size() > 0) {
+          for (LXChannel c : g.channels) {
+            if (c.enabled.getValueb()) {
+              logger.info("Child channel " + c.getLabel() + " active during clip");
+              logger.info("Active pattern: " + c.getActivePattern().label.getString() +
+                  " index: " + c.getActivePatternIndex());
+            }
+          }
+        }
+      } else if (channelBus instanceof LXChannel && channelBus.enabled.getValueb()) {
+        LXChannel c = (LXChannel) channelBus;
+        LXGroup g = c.getGroup();
+        if (g == null) {
+          logger.info("Channel " + c.getLabel() + " active during clip");
+          logger.info("Active pattern: " + c.getActivePattern().label.getString() +
+              " index: " + c.getActivePatternIndex());
+        }
+      }
+    }
+  }
+
   @Override
   public void run(double deltaMs, double amount) {
     long totalPower = 0;
@@ -36,10 +65,13 @@ public class PowerLimiter extends LXEffect {
     }
     long targetPower = (long)(maxTheoreticalPower * maxPowerPercent.getValuef());
     if (totalPower > targetPower) {
+      if (!clipDetect.getValueb()) {
+        logger.info("Clip detected!!!!!!!!");
+        logActiveChannelNames();
+      }
       clipDetect.setValue(true);
       float overLimit = (float)((double)totalPower / (double)targetPower);
       float compressAmt = 1f / overLimit;
-      //logger.info("compress target: " + compressAmt);
       for (int i = 0; i < this.colors.length; i++) {
         colors[i] = RenderImageUtil.getWeightedColor(colors[i], compressAmt);
       }
