da7daf36c947e63bcb7bc27094b0dc0ee69a2ef8
Tracy Scott <tracy.scott@gmail.com>
2021-03-29 21:40:52 -0700

    TextFxFade pattern.
    Text fades in and then fades out.  Hardcoded timing is lame but there is no more UI space.  LtoR button is re-used for enabling/disabling drop shadow.

--- src/main/java/com/giantrainbow/patterns/TextFxFade.java
+++ src/main/java/com/giantrainbow/patterns/TextFxFade.java
@@ -1,3 +1,4 @@
+
 package com.giantrainbow.patterns;
 
 import com.giantrainbow.EaseUtil;
@@ -8,14 +9,15 @@ import java.util.List;
 import java.util.logging.Logger;
 
 @LXCategory(LXCategory.FORM)
-public class TextFxZoom extends TextFx {
+public class TextFxFade extends TextFx {
   private static final Logger logger = Logger.getLogger(TextFxZoom.class.getName());
 
-  protected int currentLine = 0;
-  float scaleElapsed = 0f;
-  float scaleDuration = 4f;
+  boolean fadeIn = true;
+  float fadeElapsed = 0f;
+  float fadeDuration = 4f;
+  float fadeOutDuration = 4f;
 
-  public TextFxZoom(LX lx) {
+  public TextFxFade(LX lx) {
     super(lx);
     preRenderCh = false;
     spaceChWidth = 4;
@@ -31,17 +33,9 @@ public class TextFxZoom extends TextFx {
    * Resets the animation to initial conditions.
    */
   protected void resetAnimation() {
-    for (int j = 0; j < taDetails.spritesPerLine.size(); j++) {
-      List<CharSprite> thisLineSprites = taDetails.spritesPerLine.get(j);
-      int lineWidth = taDetails.lineWidths.get(j);
-      for (int i = 0; i < thisLineSprites.size(); i++) {
-        CharSprite chSprite = thisLineSprites.get(i);
-        // During initialization, the target positions are centered horizontally and vertically
-        chSprite.curPosX = chSprite.targetPosX;
-        chSprite.curPosY = chSprite.targetPosY;
-      }
-    }
-    scaleElapsed = 0f;
+    fadeElapsed = 0f;
+    fadeIn = true;
+    targetHoldTime = 0.5f; //500;  // Since we fade out, have base battern just had .5 seconds.
   }
 
   /**
@@ -52,7 +46,10 @@ public class TextFxZoom extends TextFx {
    */
   @Override
   public boolean drawCharacters(double deltaMs) {
-    float currentScale = EaseUtil.ease(scaleElapsed / (scaleDuration * 1000f), 4);
+    float currentFade = EaseUtil.ease(fadeElapsed / (fadeDuration * 1000f), 4);
+    if (!fadeIn)
+      currentFade = EaseUtil.ease(1f - (fadeElapsed / (fadeOutDuration * 1000f)), 4);
+
     for (int j = 0; j < taDetails.spritesPerLine.size(); j++) {
       List<CharSprite> thisLineSprites = taDetails.spritesPerLine.get(j);
       for (int i = 0; i < thisLineSprites.size(); i++) {
@@ -60,23 +57,31 @@ public class TextFxZoom extends TextFx {
         if (leftToRight.isOn()) {
           pg.pushMatrix();
           pg.translate(ch.curPosX, ch.curPosY + fontSizeKnob.getValuef());
-          pg.scale(currentScale + 0.1f);
-          pg.fill(0);
+          pg.scale(1f + 0.1f);
+          pg.fill(0, currentFade * 255f);
           pg.text(ch.ch, 0, yAdj.getValuef());
           pg.popMatrix();
         }
         pg.pushMatrix();
         pg.translate(ch.curPosX, ch.curPosY + fontSizeKnob.getValuef());
-        pg.scale(currentScale);
-        pg.fill(ch.color);
+        pg.scale(1f);
+        pg.fill(ch.color, currentFade * 255f);
         pg.text(ch.ch, 0, yAdj.getValuef());
         pg.popMatrix();
       }
     }
-    scaleElapsed += deltaMs;
-    if (scaleElapsed > (scaleDuration * 1000f)) {
-      scaleElapsed = scaleDuration * 1000f;
-      return true;
+    if (fadeIn) {
+      fadeElapsed += deltaMs;
+      if (fadeElapsed > (fadeDuration * 1000f)) {
+        fadeIn = false;
+        fadeElapsed = 0f;
+      }
+    } else {
+      fadeElapsed += deltaMs;
+      if (fadeElapsed > (fadeOutDuration * 1000f)) {
+        fadeElapsed = fadeOutDuration * 1000f;
+        return true;
+      }
     }
 
     return false;
